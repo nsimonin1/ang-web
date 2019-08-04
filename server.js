1221b3d79ec77472e14c86b9334a7ca733a9b4eb
nsimonin1 <nsimonin1@gmail.com>
2019-08-04 10:18:42 +0200

    bug fixe

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 
-app.use(express.static(__dirname+'dist/visio-logic-ui'));
+app.use(express.static(__dirname+'/dist/visio-logic-ui'));
 
 
 app.get('/*', function(req,resp){
-  resp.sendFile(path.join(__dirname+'dist/visio-logic-ui/index.html'));
+  resp.sendFile(path.join(__dirname+'/dist/visio-logic-ui/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
